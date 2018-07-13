88c93951d43b018b76ada9f22899eb4b1f9a892e
yunlong.li <yunlong.li@duobei.com>
2018-07-13 17:01:32 +0800

    提交bug修改以及done work

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main ()
 	cout << "hello world ! this is liyunlong , test the git ." << endl;
 	cout << "hello world again, this is liyunlong." << endl;
 	cout << "third to say Hello World" << endl;
-	cout << "fix this bug" << endl;
+	cout << "fix this bug, i modify something" << endl;
+	cout << "i am tring to do the work , but the work is not done." << endl;
+	cout << "i have done the work!" << endl;
 	return 0;
 }
