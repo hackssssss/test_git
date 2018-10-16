793aeab4ac74a45a486e71b3bcad3d331e1f318a
liyunlong <liyunlong@liyunlongdeMacBook-Pro.local>
2018-10-16 19:08:45 +0800

    提交test代码

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main ()
 {
+	cout << "test" << endl;"
 	cout << "hello world ! this is liyunlong , test the git ." << endl;
 	cout << "hello world again, this is liyunlong." << endl;
 	cout << "third to say Hello World" << endl;
